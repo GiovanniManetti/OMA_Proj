a6f7c6c50f53956455ee724ded85337747af596a
GiovanniManetti <manetti.giovanni.94@gmail.com>
2016-12-15 19:13:18 +0100

    Update heuristic.c

--- heuristic.c
+++ heuristic.c
@@ -1,3 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "heuristic.h"
+
+struct Data {
+    /**
+     * The costs ijkt
+     */
+    int**** costs;
+
+    /**
+     * number of activity done by one user of type k
+     */
+    int* n;
+
+    /**
+     * Activities to be done in node i during the given time horizon
+     */
+    int* activities;
+
+    /**
+     * Number of users of type m in i during time period t (ikt)
+     */
+    int*** usersCell;
+};
+
+void createRandSol(int****,struct Data,int*,int,int,int);
